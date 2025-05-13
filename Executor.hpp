5ec793617b660f235ab69320fc23dcba902fcf22
Piotrek Rybiec <piotrrybiec28@gmail.com>
2025-05-14 01:59:58 +0200

    execuuuuutooooor

---
+++ Executor.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "Commands.hpp"
+#include "Database.h"
+
+class Executor {
+private:
+    Database& database_;
+
+    void executeSelect(const SelectCommand& command);
+    void executeCreate(const CreateCommand& command);
+    void executeDrop(const DropCommand& command);
+    void executeInsert(const InsertCommand& command);
+    void executeUpdate(const UpdateCommand& command);
+    void executeDelete(const DeleteCommand& command);
+
+public:
+    explicit Executor(Database& database) : database_(database) {}
+
+    void execute(const std::unique_ptr<Command>& command);
+};
